4119cb7a87f6cdc23c9db5014166c831bdc4986d
ashjack <ashleymac1999@gmail.com>
2023-06-20 17:48:53 +0100

    Add missing project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HexButtonComponent } from './hex-button/hex-button.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HexButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
