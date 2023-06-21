dfdc6fee4caadb69bbd5cd69439ce3758a19b7ff
ashjack <ashleymac1999@gmail.com>
2023-06-21 20:30:16 +0100

    Added interactive timeline component

---
+++ src/app/components/timeline/timeline.component.ts
@@ -0,0 +1,58 @@
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { Job } from '../../models';
+import { HttpClient } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.css']
+})
+export class TimelineComponent {
+  history: Job[] = [];
+  selectedJob?: Job;
+
+  constructor(private http: HttpClient, private elRef: ElementRef, private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+    //Fill out the history array with JSON data from assets/history.json
+    this.http.get('assets/history.json').subscribe(data => {
+      console.log(data);
+      this.history = data as Job[];
+    });
+  }
+
+  openModal(content: any, job: Job): void {
+    if (job) {
+      this.selectedJob = job;
+      this.modalService.open(content, { size: 'lg', centered: true });
+    }
+  }
+
+  closeModal(modal: any): void {
+    modal.close('Close click');
+    this.selectedJob = undefined;
+  }
+
+  @HostListener('window:scroll')
+  checkScroll(): void {
+    const elements = this.elRef.nativeElement.querySelectorAll('.circle');
+    elements.forEach((element: HTMLElement) => {
+      const rect = element.getBoundingClientRect();
+
+      // Circle is visible in the viewport, add animation class
+      if (rect.top < window.innerHeight && rect.bottom >= 0)
+      {
+        const jobText = element.querySelector('.job-text') as HTMLElement;
+        jobText.classList.add('fade-slide-in');
+      }
+
+      // Circle is not visible, remove animation class
+      else
+      {
+        const jobText = element.querySelector('.job-text') as HTMLElement;
+        jobText.classList.remove('fade-slide-in');
+      }
+    });
+  }
+}
