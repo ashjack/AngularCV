ab32e457f7b8e0dbceb0edbcd618b964b3b364c7
ashjack <ashleymac1999@gmail.com>
2023-06-21 23:55:50 +0100

    Combined components into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HexButtonComponent } from './hex-button/hex-button.component';
-import { HeaderComponent } from './header/header.component';
+import { HexButtonComponent } from './components/hex-button/hex-button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgbModalModule, NgbModule, NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { HexCollectionComponent } from './components/hex-collection/hex-collection.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { CareerProfileComponent } from './components/career-profile/career-profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HexButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    HexCollectionComponent,
+    TimelineComponent,
+    SkillsComponent,
+    CareerProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgbProgressbarModule,
+    NgbModalModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
